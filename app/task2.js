0fbc32344b8f7e07306e2e684695cef08abcde3c
Михаил Вялов <accmi@MacBook-Pro-Mihail.local>
2019-11-28 00:45:48 +0300

    fix after review

--- app/task2.js
+++ app/task2.js
@@ -1,24 +1,47 @@
-import * as fs from "fs";
-import csvtojson from "csvtojson";
+import * as fs from 'fs';
+import * as readline from 'readline';
+// TODO: Узнать как обработывать кажду строку при чтении файла CSV
+// import csvtojson from "csvtojson";
+import { Converter } from './src/controllers/Converter';
 import * as path from 'path';
 import * as uuid from 'uuid';
 
 const fileReadPath = path.join(__dirname, '../csv/task2.csv');
+const folderName = 'filesResult';
+const folderWritePath = path.join(__dirname, `../csv/${folderName}/`);
+
+if (!fs.existsSync(folderWritePath)) {
+    fs.mkdirSync(folderWritePath);
+}
 const fileName = `${uuid.v1()}.txt`;
-const fileWritePath = path.join(__dirname, `../csv/filesResult/${fileName}`);
+const fileWritePath = `${folderWritePath}${fileName}`;
 
-const readStream = fs.createReadStream(fileReadPath);
-const writeStream = fs.createWriteStream(fileWritePath);
-const converter = csvtojson();
+const converter = new Converter({
+    ignoreColl: /Amount/
+});
 
-readStream
-    .pipe(converter)
-    .pipe(writeStream);
+const rl = readline.createInterface({
+    input: fs.createReadStream(fileReadPath),
+    output: fs.createWriteStream(fileWritePath),
+});
 
-readStream
-    .on('error', (err) => {
-        console.error(`We have a error during wrting/reading file\n${err}`);
+rl.on('line', (line) => rl.emit('pause', line));
+rl.on('pause', (line) => {
+    if (!converter.isHeaderAdd) {
+        converter.setHeader(line);
+
+        return;
+    }
+
+    converter.convert(line, (row) => {
+        rl.output.write(`${row}\n`);
     })
-    .on('end', () => {
-        console.log(`Created ${writeStream.path}`);
-    });
+});
+
+rl.on('close', () => {
+    console.log(`Created ${rl.output.path}`);
+});
+
+rl.on('error', (err) => {
+    console.log(err);
+});
